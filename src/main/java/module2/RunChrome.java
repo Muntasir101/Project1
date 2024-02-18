aaa9df205cffd4e91d702251a78f73db44dc5d57
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-02-18 11:45:43 +0600

    Add Comments

--- src/main/java/module2/RunChrome.java
+++ src/main/java/module2/RunChrome.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RunChrome {
     public static void main(String[] args) {
+        // Create a new instance of the ChromeDriver
         WebDriver driver = new ChromeDriver();
+        // This will open a new Chrome browser window
     }
 }

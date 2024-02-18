66b4a0fa8f1be11e61024e4963dcf147bd87ffff
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-02-18 11:39:13 +0600

    Project setup and Browser launch

---
+++ src/main/java/module2/RunFirefox.java
@@ -0,0 +1,9 @@
+package module2;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class RunFirefox {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+}

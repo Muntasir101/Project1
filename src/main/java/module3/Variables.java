03c2190d80474446ac6a2c745d60618fdba4a95d
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-02-18 12:57:52 +0600

    variables

---
+++ src/main/java/module3/Variables.java
@@ -0,0 +1,31 @@
+package module3;
+
+public class Variables {
+    // Class variables
+    public static int number1 = 50;
+
+    public static void main(String[] args) {
+      // Integer Type
+      byte myByte = 100;
+      short myShort =  10;
+      int age = 2000;
+      float tax = 10.5f;
+      double tax2 = 10.5;
+
+      // Boolean
+      boolean login_Status = false;
+
+      // Character
+      char name = 'a';
+
+      // String
+        String MyName = "Smith";
+
+      System.out.println(age);
+
+      System.out.println(number1);
+
+      // 1-D
+      int[] numbers = new int[5];
+    }
+}
